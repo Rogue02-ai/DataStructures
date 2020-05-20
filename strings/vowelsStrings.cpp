de5db343c338dcd85c48f0a25988bfc2c3734fc2
Rogue02-ai <shantanr@umich.edu>
2020-05-20 12:41:37 -0400

    modify

--- strings/vowelsStrings.cpp
+++ strings/vowelsStrings.cpp
@@ -8,11 +8,13 @@ int main()
     int i, vcount = 0, ccount = 0;
     for (i = 0; A[i] != '\0'; i++)
     {
+        //checking for vowels
         if (A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u' ||
             A[i] == 'A' || A[i] == 'E' || A[i] == 'I' || A[i] == 'O' || A[i] == 'U')
         {
             vcount++;
         }
+        //checking for consonants
         else if ((A[i] >= 65 && A[i] <= 90) || (A[i] >= 97 && A[i] <= 122))
         {
             ccount++;
