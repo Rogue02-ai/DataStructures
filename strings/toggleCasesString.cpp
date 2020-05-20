850ad80b29e9729376ab8dae341ff101a9f8d53a
Rogue02-ai <shantanr@umich.edu>
2020-05-20 12:16:21 -0400

    added code

--- strings/toggleCasesString.cpp
+++ strings/toggleCasesString.cpp
@@ -1,1 +1,23 @@
-// toggling cases in a given string.
+// toggling cases in a given string (without spaces).
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char A[] = "AnalysisANDDesignOFAlgorithm";
+    int i;
+    cout << "Original: " << A << endl;
+    for (i = 0; A[i] != '\0'; i++)
+    {
+        if (A[i] >= 65 && A[i] <= 90)
+        {
+            A[i] = A[i] + 32;
+        }
+        else if (A[i] >= 97 && A[i] <= 122)
+        {
+            A[i] = A[i] - 32;
+        }
+    }
+    cout << "Toggled case: " << A;
+}
