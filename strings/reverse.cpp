34f1a971d24a2f434866dab32515eaeddce29faa
Rogue02-ai <shantanr@umich.edu>
2020-05-24 22:56:00 -0400

    modify

--- strings/reverse.cpp
+++ strings/reverse.cpp
@@ -1,3 +1,5 @@
+// ocde to reverse a string.
+
 #include <iostream>
 using namespace std;
 
@@ -7,10 +9,12 @@ int main()
     int size = sizeof(A) / sizeof(A[0]);
     char B[size] = "";
     int i, j;
+    //moving i to the end of the char A[].
     for (i = 0; A[i] != '\0'; i++)
     {
     }
     i = i - 1;
+    //copying value of A[] to B[].
     for (j = 0; i >= 0; i--, j++)
     {
         B[j] = A[i];
