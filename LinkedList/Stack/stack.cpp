ead6b93f99ecf3091704329c78b713809da2c48b
Rogue02-ai <shantanr@umich.edu>
2020-08-11 17:07:17 -0400

    added file

---
+++ LinkedList/Stack/stack.cpp
@@ -0,0 +1,115 @@
+# include <iostream>
+using namespace std;
+
+template<class T>
+class Stack {
+private:
+    T *st;
+    int top;
+    int size;
+
+public:
+    Stack() {
+        size = 10;
+        top = -1;
+        st = new T[size];
+    }
+
+    Stack(int size) {
+        this->size = size;
+        top = -1;
+        st = new T[this->size];
+    }
+
+    void push(T x);
+    T pop();
+    T peek(int index);
+    int stackTop();
+    int isEmpty();
+    int isFull();
+    void display();
+};
+
+template <class T>
+void Stack<T>::push(T x) {
+    if (isFull()) {
+        cout<<"Stack Overflow";
+    }
+    else
+    {
+        top++;
+        st[top] = x;
+    }
+}
+
+template <class T>
+T Stack<T>::pop() {
+    T x = -1;
+    if (isEmpty()) {
+        cout<<"Stack underflow";
+    }
+    else {
+        x = st[top];
+        top--;
+    }
+    return x;
+}
+
+template<class T>
+T Stack<T>::peek(int index) {
+    T x = -1;
+    if (top-index+1<0) {
+        cout<<"Invalid Index"<<endl;
+    }
+    else {
+        x = st[top-index+1];
+    }
+    return x;
+}
+
+template<class T>
+int Stack<T>::stackTop() {
+    if (isEmpty()) {
+        return -1;
+    }
+    return st[top];
+}
+
+
+template<class T>
+int Stack<T>::isEmpty() {
+    return top == -1;
+}
+
+template<class T>
+int Stack<T>::isFull() {
+    return top == size-1;
+}
+
+template <class T>
+void Stack<T>::display() {
+    for (int i =top; i>=0;i--) {
+        cout<<st[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main() {
+
+    Stack<char> stk(4);
+    stk.push('a');
+    stk.push('b');
+    stk.push('c');
+    stk.push('d');
+
+    stk.display();
+    cout<<stk.stackTop()<<endl;
+    cout<<stk.peek(1)<<endl;
+
+    stk.pop();
+    cout<<stk.stackTop()<<endl;
+    stk.display();
+
+    return 0;
+}
+
